43cf86580058d11c07f88c0867506b2ac6866d12
Guilherme Carvalho <guilherme.carvalho@nuvols.com.br>
2020-08-04 07:18:35 -0300

    Refactoring code

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flutter_cli',
-    version='0.0.3',
+    version='0.1.0',
     packages=['cli_flutter', 'snippets'],
     include_package_data=True,
     entry_points={
